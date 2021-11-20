e2e34fb98403662b4811232c8aefa7600f96b6d4
Kayla Ousley <kayousley22@gmail.com>
2021-11-20 16:59:11 -0500

    created links and views for forums, listings, profile, and edit profile pahges.

--- rcjourney/rcapp/urls.py
+++ rcjourney/rcapp/urls.py
@@ -5,7 +5,13 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    
+
     path('', views.landing, name='landing'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('profile/', views.profile, name='profile'),
+    path('editprofile/', views.profile, name='editprofile'),
+    path('forums/', views.profile, name='forums'),
+    path('listings/', views.profile, name='listings'),
+
 ]
