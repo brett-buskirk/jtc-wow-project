6bc0878abc9ac8a596fd0657ab0bd01a9a064929
Joe Bollinger <joeboo865@gmail.com>
2021-12-21 12:37:26 -0500

    changed search funcionality on forum page and added paganation to profile page

--- rcjourney/rcapp/forms.py
+++ rcjourney/rcapp/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import *
-# from taggit.forms import TagField
 
 
 class CreateUserForm(UserCreationForm):
@@ -19,6 +18,7 @@ class EditProfileForm(ModelForm):
         fields = '__all__'
         exclude = ['user']
 
+
 class PostForm(ModelForm):
     class Meta:
         model = Post
@@ -27,10 +27,8 @@ class PostForm(ModelForm):
 
 class PostForm(forms.Form):
     post = forms.CharField(widget=forms.Textarea, required=True)
-    #tag = forms.CharField(widget=forms.CharField)
     choices = []
     for tag in Tag.objects.all():
         choices.append((tag.tag_id, tag.name))
     tags = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, choices=choices, required=True)
-    # tags = TagField(label="Tags")
